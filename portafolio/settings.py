35552ae0bb10248e86085ab8c121402d732bea67
zurima taimara <aladejotaimara@gmail.com>
2025-02-08 10:10:22 -0400

    add styles to CRUD

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATIC_ROOT= BASE_DIR/'static/'
+STATICFILES_DIRS=[BASE_DIR/"static"]
 
 MEDIA_ROOT= BASE_DIR/ "media"
 MEDIA_URL="media/"
