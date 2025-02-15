a622de6b036d831e57dea8dbdd4c47afbe9d467a
zurima taimara <aladejotaimara@gmail.com>
2025-02-15 10:24:30 -0400

    add login feature

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include 
 from authentication.views import *
 
 urlpatterns=[
     path("",SignUpView.as_view(),name="signup"),
+    path("accounts/", include("django.contrib.auth.urls"))
 ]
